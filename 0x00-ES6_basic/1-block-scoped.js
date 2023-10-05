71ad998752dd785a9edcabbc53fc7ca84cb80c3f
Scolar02 <njugunascolar83@gmail.com>
2023-10-05 13:21:32 +0000

    task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,9 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  if (trueOrFalse) {
-    // You don't need to redeclare task and task2 here
-    // task = true;   // Remove this line
-    // task2 = false; // Remove this line
+  if (trueOrFals) {
+    // task = true; //
+    // task2 = false; //
   }
 
   return [task, task2];
