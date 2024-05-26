0e4268b718d67da728ab40d8e3355931ec8ecd77
Scolar02 <njugunascolar83@gmail.com>
2024-05-26 13:56:27 +0000

    Rewrite the getBudgetForCurrentYear function to use ES6 computed property names on the budget object

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+
+rt default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
+  for (const idx of array) {
+    const value = idx;
+    arr.push(appendString + value);
+  }
+
+  return arr;
+}
