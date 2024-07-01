c3f8dd06d55254028f09d4b1a4a9347c6adfb549
Scolar02 <njugunascolar83@gmail.com>
2024-07-01 07:36:31 +0000

    ES6 promise basics

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 // 0-promise.js
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    // The function resolves immediately for this example
     resolve();
   });
 }
+
