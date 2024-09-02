410471eabfce652fcab0542bdf3503236b3ad249
Scolar02 <njugunascolar83@gmail.com>
2024-09-02 09:12:38 +0000

    Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,30 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  let consoleSpy;
+
+  beforeEach(() => {
+    // Create a spy for console.log before each test
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    // Restore the original function after each test
+    consoleSpy.restore();
+  });
+
+  it('should log "The total is: 120" when called with 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+  });
+
+  it('should log "The total is: 20" when called with 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+  });
+});
